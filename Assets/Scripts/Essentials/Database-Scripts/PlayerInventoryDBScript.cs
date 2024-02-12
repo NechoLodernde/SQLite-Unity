29a7ccc8ccfd276a69f5ff03073c94605a098946
Jovian Kurnia <jovian1gdf@gmail.com>
2024-02-12 16:55:18 +0700

    Update PlayerInventoryDBScript.cs
    - Added functionality

--- Assets/Scripts/Essentials/Database-Scripts/PlayerInventoryDBScript.cs
+++ Assets/Scripts/Essentials/Database-Scripts/PlayerInventoryDBScript.cs
@@ -162,7 +162,24 @@ public class PlayerInventoryDBScript : MonoBehaviour
     {
         using (dbConnect = new SqliteConnection(conn))
         {
+            dbConnect.Open();
+            dbCommand = dbConnect.CreateCommand();
+            PlayerInventoryEntry[] rawData = ReturnPIEArray();
 
+            foreach (PlayerInventoryEntry entry in rawData)
+            {
+                sqlQuery = "UPDATE playerinventory SET inventory_data_code='" +
+                    entry.inventoryID + "', player_inventory_number='" +
+                    entry.inventoryNumber + "', player_inventory_name='" +
+                    entry.inventoryName + "', inventory_usable_code='" +
+                    entry.inventoryUsableCode + "' WHERE inventory_data_code='" +
+                    entry.inventoryID + "';";
+
+                dbCommand.CommandText = sqlQuery;
+                dbCommand.ExecuteScalar();
+            }
+
+            dbConnect.Close();
         }
     }
 
@@ -170,7 +187,149 @@ public class PlayerInventoryDBScript : MonoBehaviour
     {
         using (dbConnect = new SqliteConnection(conn))
         {
+            dbConnect.Open();
+            dbCommand = dbConnect.CreateCommand();
+            sqlQuery = "SELECT * FROM playerinventory";
+            dbCommand.CommandText = sqlQuery;
+            dbReader = dbCommand.ExecuteReader();
 
+            while (dbReader.Read())
+            {
+                PlayerInventoryEntry newEntry = new();
+                string pIC, pIName;
+                int pINumber, iUCode;
+                pIC = "" + dbReader["inventory_data_code"];
+                pIName = "" + dbReader["player_inventory_name"];
+
+                int.TryParse(("" + dbReader["player_inventory_number"]),
+                    out int x);
+                pINumber = x;
+                int.TryParse(("" + dbReader["inventory_usable_code"]),
+                    out int y);
+                iUCode = y;
+
+                Debug.Log("The Data:");
+                Debug.Log("Player Inventory Code: " + pIC);
+                Debug.Log("Player Inventory Name: " + pIName);
+                Debug.Log("Player Inventory Number: " + pINumber);
+                Debug.Log("Inventory Usable Code: " + iUCode);
+                Debug.Log("End of Data");
+
+                newEntry.inventoryID = pIC;
+                newEntry.inventoryNumber = pINumber;
+                newEntry.inventoryName = pIName;
+                newEntry.inventoryUsableCode = iUCode;
+
+                PlayerInventoryManager.Instance.Struct.list.Add(newEntry);
+            }
+
+            dbReader.Close();
+        }
+
+        dbConnect.Close();
+    }
+
+    public void UpdatePID(string prevPID, string newPID)
+    {
+        using (dbConnect = new SqliteConnection(conn))
+        {
+            dbConnect.Open();
+            dbCommand = dbConnect.CreateCommand();
+            PlayerInventoryEntry[] rawData = ReturnPIEArray();
+
+            foreach (PlayerInventoryEntry entry in rawData)
+            {
+                if (entry.inventoryID.Equals(prevPID))
+                {
+                    sqlQuery = "UPDATE playerinventory SET inventory_data_code='" +
+                        newPID + "' WHERE inventory_data_code='" + prevPID + "';";
+
+                    PlayerInventoryManager.Instance.UpdateInvID(prevPID, newPID);
+                    dbCommand.CommandText = sqlQuery;
+                    dbCommand.ExecuteScalar();
+                    break;
+                }
+            }
+
+            dbConnect.Close();
+        }
+    }
+
+    public void UpdatePINumber(string prevPID, int newPIN)
+    {
+        using (dbConnect = new SqliteConnection(conn))
+        {
+            dbConnect.Open();
+            dbCommand = dbConnect.CreateCommand();
+            PlayerInventoryEntry[] rawData = ReturnPIEArray();
+
+            foreach (PlayerInventoryEntry entry in rawData)
+            {
+                if (entry.inventoryID.Equals(prevPID))
+                {
+                    sqlQuery = "UPDATE playerinventory set player_inventory_number='" +
+                        newPIN + "' WHERE inventory_data_code='" + prevPID + "';";
+
+                    PlayerInventoryManager.Instance.UpdateInvNumber(prevPID, newPIN);
+                    dbCommand.CommandText = sqlQuery;
+                    dbCommand.ExecuteScalar();
+                    break;
+                }
+            }
+
+            dbConnect.Close();
+        }
+    }
+
+    public void UpdatePIName(string prevPID, string newPIName)
+    {
+        using (dbConnect = new SqliteConnection(conn))
+        {
+            dbConnect.Open();
+            dbCommand = dbConnect.CreateCommand();
+            PlayerInventoryEntry[] rawData = ReturnPIEArray();
+
+            foreach (PlayerInventoryEntry entry in rawData)
+            {
+                if (entry.inventoryID.Equals(prevPID))
+                {
+                    sqlQuery = "UPDATE playerinventory set player_inventory_name='" +
+                        newPIName + "' WHERE inventory_data_code='" + prevPID + "';";
+
+                    PlayerInventoryManager.Instance.UpdateInvName(prevPID, newPIName);
+                    dbCommand.CommandText = sqlQuery;
+                    dbCommand.ExecuteScalar();
+                    break;
+                }
+            }
+
+            dbConnect.Close();
+        }
+    }
+
+    public void UpdateIUCode(string prevPID, int newIUCode)
+    {
+        using (dbConnect = new SqliteConnection(conn))
+        {
+            dbConnect.Open();
+            dbCommand = dbConnect.CreateCommand();
+            PlayerInventoryEntry[] rawData = ReturnPIEArray();
+
+            foreach (PlayerInventoryEntry entry in rawData)
+            {
+                if (entry.inventoryID.Equals(prevPID))
+                {
+                    sqlQuery = "UPDATE playerinventory set inventory_usable_code='" +
+                        newIUCode + "' WHERE inventory_data_code='" + prevPID + "';";
+
+                    PlayerInventoryManager.Instance.UpdateUsableCode(prevPID, newIUCode);
+                    dbCommand.CommandText = sqlQuery;
+                    dbCommand.ExecuteScalar();
+                    break;
+                }
+            }
+
+            dbConnect.Close();
         }
     }
 
